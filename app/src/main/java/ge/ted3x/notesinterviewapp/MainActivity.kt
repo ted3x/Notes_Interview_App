8d9d4ebf4ac23c44caebc5b1d052f7eef4beedce
Tedo Manvelidze <67691418+ted3x@users.noreply.github.com>
2024-05-22 17:54:46 +0400

    Initial commit

---
+++ app/src/main/java/ge/ted3x/notesinterviewapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package ge.ted3x.notesinterviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Note Model
+ *
+ * id - String
+ * title - String
+ * type - [ImageNote, TextNote]
+ *
+ * Image Note Model
+ *
+ * image - String(url)
+ *
+ * Text Note
+ *
+ * text - String
+ *
+ * https://run.mocky.io/v3/f9976553-941e-482e-9659-9deb70694610
+ *
+ *
+ *
+ * Implement CRUD operations ( Add, Update, Get(All), Delete )
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
